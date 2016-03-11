a8650725749bfac69bae9751549ca2b82313d450
TanyaEf <efimenkot@gmail.com>
2016-03-11 15:38:23 +0200

    Initial commit

---
+++ src/main/java/com/jaspersoft/jasperserver/dsa/Application.java
@@ -0,0 +1,15 @@
+package com.jaspersoft.jasperserver.dsa;
+
+/**
+ * <p/>
+ * <p/>
+ *
+ * @author tetiana.iefimenko
+ * @version $Id$
+ * @see
+ */
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
